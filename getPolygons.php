c8f30d4da944fb3113cf9124c426e9d17bc669bd
OldSaltyFish-HJ <hejing9973@126.com>
2020-07-07 22:31:29 +0800

    配合服务端危险区域标红部分修改了一下

--- getPolygons.php
+++ getPolygons.php
@@ -1,6 +1,6 @@
 <?php
 	include (dirname(__FILE__).'/private_version/connect.php');
-	$sql = "select polygonId, latitude, longitude from points order by pointId";
+	$sql = "select points.polygonId, latitude, longitude, risk from points left join polygons on points.polygonId = polygons.polygonId order by pointId";
 	if ($result = mysql_query($sql)) {
 		$arr = Array();
 		while ($rows = mysql_fetch_array($result,MYSQL_ASSOC)){
